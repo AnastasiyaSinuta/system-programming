bff26d6a7a2c4a37631d71117ac8b85e48cd9a1a
Anastasiya Sinuta <anastasiya.sinyuta2003@gmail.com>
2023-05-31 01:04:51 +0300

    add some options

--- lab12aasN32511/lab1aasN32511.c
+++ lab12aasN32511/lab1aasN32511.c
@@ -3,22 +3,21 @@
 #include <string.h>
 
 int debug_mode();
+void options(int, char*, char*);
 char* convertToDecimal(char*);
 void walk_dir(int, char*, char*);
 
 int main(int argc, char *argv[]) {
     int debug = debug_mode();
-    if (argc != 3) { 
-        printf("Using: %s <dir> <target>\n", argv[0]); 
-        return EXIT_FAILURE; 
+    if (argc < 2) {
+        fprintf(stderr, "Using: %s <dir> <target>\n", argv[0]);
+        return EXIT_FAILURE;
     }
-    else {
-        char* decimal = convertToDecimal(argv[2]);
-        if (debug) {
-            printf("The target %s in decimal notation: %s\n", argv[2], decimal); 
-        }
-        walk_dir(debug, argv[1], decimal);
+    for (int i = 1; i < argc; i++) {
+        options(debug, (char*)argv[0], (char*)argv[i]);
     }
-    
+    char* decimal = convertToDecimal(argv[2]);
+    if (debug) fprintf(stderr, "The target %s in decimal notation: %s\n", argv[2], decimal);
+    walk_dir(debug, argv[1], decimal);
     return EXIT_SUCCESS;
 }
---
+++ lab12aasN32511/options.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_version(int);
+void print_help(int, const char*);
+void plugins_dir();
+void and_operation();
+void or_operation();
+
+void options(int debug, char* name, char* option) {
+    if (strcmp(option, "-v") == 0) {
+        print_version(debug);
+    }
+    else if (strcmp(option, "-h") == 0) {
+        print_help(debug, name);
+    }
+    else if (strcmp(option, "-P") == 0) {
+        plugins_dir();
+    }
+    else if (strcmp(option, "-A") == 0) {
+        and_operation();
+    }
+    else if (strcmp(option, "-O") == 0) {
+        or_operation();
+    }
+    else return;
+    if (debug) fprintf(stderr, "debug: End debugging.\n");
+    exit(EXIT_SUCCESS);
+}
+
+void print_version(int debug) {
+    if (debug) fprintf(stderr, "debug: \"Version\" option.\n");
+    char *Version = "1.0";
+    const char *Autor = "Sinuta Anastasiya Anatolevna";
+    const char *Group = "N32511";
+    const int Variant = 5;
+    printf("Version: %s\nAutor: %s\nGroup: %s\nVariant: %d\n", Version, Autor, Group, Variant);
+}
+
+void print_help(int debug, const char* name) {
+    if (debug) fprintf(stderr, "debug: \"Help\" option.\n");
+    printf("Usage: %s [options] [directory] [target]...\n", name);
+    printf("\nOptions:\n");
+    const char* options[] = {
+        "\t-h Print this message and exit.\n",
+        "\t-v Print the version, autor, group, variant and exit.\n",
+        "\t-P <dir>.\n",
+        "\t-A <dir>.\n",
+        "\t-O <dir>.\n"
+    };
+    int count = sizeof(options)/sizeof(options[0]);
+    for (int i = 0; i < count; i++) {
+        printf("%s", options[i]);
+    }
+    printf("\nDirectory:\n");
+    printf("\tSpecify the byte sequences to search. The target argument is in the format of an encoded string UTF-8.\n");
+    printf("\nTarget:\n");
+    printf("\tSpecify the path to the directory, starting from the directory \'/home\', from which you want to start the search.\n");
+    printf("\nThis program built for x86_64-pc-linux-gnu\n");
+    printf("Report bugs to <336972@niuitmo.ru>\n");
+}
+
+void plugins_dir() {
+    printf("Еще не сделано\n");
+}
+
+void and_operation() {
+    printf("Еще не сделано\n");
+}
+
+void or_operation() {
+    printf("Еще не сделано\n");
+}
