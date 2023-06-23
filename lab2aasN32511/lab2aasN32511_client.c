477976abd161c2e5b9c0a3f913eacbf3a8993c71
AnastasiyaSinuta <anastasiya.sinyuta2003@gmail.com>
2023-06-24 00:50:50 +0300

    Initial lab2aasN32511

---
+++ lab2aasN32511/lab2aasN32511_client.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    //char* LAB2PORT = getenv("LAB2PORT");
+    char* LAB2DEBUG = getenv("LAB2DEBUG");
+    if (LAB2DEBUG) fprintf(stderr, "LAB2DEBUG: Debug mode enabled.\n");
+
+    for (int i = 0; i < argc; i++) {
+        if (!strcmp(argv[i], "-v")) {
+            if (LAB2DEBUG) fprintf(stderr, "LAB2DEBUG: --Version option called.\n");
+            fprintf(stderr, "Lab №2 - \"Processes, threads, and interprocess communication\"\n");
+            fprintf(stderr, "Version: 1.0\n");
+            fprintf(stderr, "Autor: Sinuta Anastasiya Anatolevna\n");
+            fprintf(stderr, "Group: N32511\n");
+            fprintf(stderr, "Variant: 25\n");
+            if (LAB2DEBUG) fprintf(stderr, "LAB2DEBUG: End debugging.\n");
+            exit(EXIT_SUCCESS);
+        }
+        if (!strcmp(argv[i], "-h")) {
+            if (LAB2DEBUG) fprintf(stderr, "LAB2DEBUG: --Help option called.\n");
+            fprintf(stderr, "Usage: %s [options]\n", argv[0]);
+            fprintf(stderr, "Options:\n");
+            fprintf(stderr, "-a ip (LAB2ADDR) Listening address server and to which client connects.\n");
+            fprintf(stderr, "-p port (LAB2PORT) Port on which it listens server and to which client connects.\n");
+            fprintf(stderr, "-v Displaying the program version.\n");
+            fprintf(stderr, "-h Display help for options.\n");
+            if (LAB2DEBUG) fprintf(stderr, "LAB2DEBUG: End debugging.\n");
+            exit(EXIT_SUCCESS);
+        }
+    }
+    return EXIT_SUCCESS;
+}
